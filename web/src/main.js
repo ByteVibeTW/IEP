0be5495c905b252e846b023c550e058a9e6aea0c
yang-lin94 <casongg@gmail.com>
2025-09-29 16:38:37 +0800

    fix: simplify silent SSO check and remove error handling
    - Removed the Content-Security-Policy meta tag from silent-check-sso.html for streamlined functionality.
    - Simplified the silent SSO check script by directly posting the message without error handling.
    - Updated main.js to disable silentCheckSsoFallback and checkLoginIframe for a cleaner authentication process.

--- web/src/main.js
+++ web/src/main.js
@@ -25,8 +25,6 @@ keycloak
     .init({
         onLoad: 'check-sso',
         silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-        silentCheckSsoFallback: false,
-        checkLoginIframe: false,
     })
     .then((authenticated) => {
         console.log(authenticated ? '用戶已登入' : '用戶未登入');
